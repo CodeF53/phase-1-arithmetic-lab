6c521b03f86b5358ba811dcccbab1107f6eb7ae7
ChaseWelsh <fseusb@gmail.com>
2022-08-08 15:23:47 -0600

    commit tree ad38b388bcdbc2e00a296b8784ed752b12f3a592

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+/*
 
-// Write your code here
+    Create a variable called multiply set to an equation that will multiply the variables num1 and num2; the result of the multiplication should be 62.
+    Create a variable called random that will generate a random integer greater than 0.
+    Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+    Create a variable called max that finds the highest number in a set; the value returned should be 20.
+
+*/
+let num1=1;
+let num2=62;
+
+let num3=14;
+let num4=10;
+
+let set = [1,2,3,20,15,7];
+
+const multiply = num1*num2;
+const random = parseInt(Math.random()*10)+1;
+const mod = num3%num4;
+const max = Math.max(...set);
